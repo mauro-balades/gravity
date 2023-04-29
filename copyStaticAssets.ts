a8cec7dc60cc8f7d454aa4d04cd32bfab219fbe3
mauro-balades <mauro.balades@tutanota.com>
2023-04-30 00:03:04 +0200

    Tested imported scripts and added message to static asset coping

--- copyStaticAssets.ts
+++ copyStaticAssets.ts
@@ -1,6 +1,8 @@
 const shell = require("shelljs");
 
+console.log("Copy static assets...")
+
 shell.rm('-rf', 'dist/ui')
 shell.cp("-R", "src/ui", "dist/ui/");
 
-console.log("Done\n")
+console.log("Done.\n")
